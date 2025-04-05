af4d5bd6d9da56990d354ccbcbae5df24fdadb3d
grandodanimar <147069937+grandodanimar@users.noreply.github.com>
2025-04-05 13:59:12 -0300

    Update webapp_teste.py

--- webapp_teste.py
+++ webapp_teste.py
@@ -164,6 +164,7 @@ def exporta_xml(files):
                         'Qtde_Trib':qtdeTrib,
                         'Vlr_Unit': vlrUnit,
                         'Vlr_Produto': vprod,
+                        'Vlr_Desconto': vdesc,
                         'BC_ICMS': vBC_icms_prop,
                         'Per_ICMS': pICMS_prop,
                         'vlr_ICMS': vICMS_prop,
@@ -194,7 +195,7 @@ def exporta_xml(files):
 
     # Converter colunas numéricas
     cols_num_float = ['BC_ICMS','BC_ICMS61','vlr_ICMS61' ,'vlr_ICMS','BC_ICMS10', 'vlr_ICMS10', 'Vlr_Produto','BC_Pis', 'Per_Pis','Vlr_Pis','BC_Cofins',
-                  'Per_Cofins', 'Vlr_Cofins', 'Per_MVAST','Vlr_Unit', 'Qtde_Trib']
+                  'Per_Cofins', 'Vlr_Cofins', 'Per_MVAST','Vlr_Unit', 'Qtde_Trib', 'Vlr_Desc]
     df[cols_num_float]= df[cols_num_float].apply(lambda col: pd.to_numeric(col, errors='coerce').round(2))
 
     cols_int = ['Per_ICMS','Per_ICMS10']
