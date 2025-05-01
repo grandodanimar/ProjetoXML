bd65b46f3d2c4409ad537765cc6ca7c3e54d9e0c
grandodanimar <147069937+grandodanimar@users.noreply.github.com>
2025-05-01 17:27:31 -0300

    Update 1-paginas.py

--- 1-paginas.py
+++ 1-paginas.py
@@ -1,6 +1,6 @@
 import streamlit as st
 
 pagina_1 = st.Page("2-leitor_xml.py", title="Converter Arquivos XML para Tabela")
-pagina_2 = st.Page("3-leitor_pdf.py", title="Lendo Arquivos PDF")
+pagina_2 = st.Page("3-leitor_pdf.py", title="Conferência Autorizações de Uso NFe")
 pg = st.navigation([pagina_1, pagina_2])
 pg.run()
