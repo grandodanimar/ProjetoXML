fdea0ec370a2af3b52470f835e836515f03e3a26
grandodanimar <147069937+grandodanimar@users.noreply.github.com>
2025-07-17 23:27:31 -0300

    Update 2-leitor_xml_com_duckdb.py

--- 2-leitor_xml_com_duckdb.py
+++ 2-leitor_xml_com_duckdb.py
@@ -442,7 +442,7 @@ if "df" in st.session_state and st.session_state.df is not None:
                                VLR_Cofins_Calc
                         FROM df
                         """).df(), hide_index=True)
-        tab3.write(duckdb.sql("SELECT Nome_Produto, NCM FROM df").df())
+        # tab3.write(duckdb.sql("SELECT Nome_Produto, NCM FROM df").df())
 
 
     
